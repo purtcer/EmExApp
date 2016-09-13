406b0a84bee3a3fce5947fbc26b3ba4d15b7e29c
Пуртов Сергей <purtcer@mail.ru>
2016-09-13 22:33:14 +0300

    Создание проекта

---
+++ EmexApp/Consumer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmexApp
+{
+    class Consumer
+    {
+        public int login;
+        public string password;
+        public Consumer()
+        {
+            login = Properties.Settings.Default.Login;
+            password = Properties.Settings.Default.Password;
+        }
+
+        public void SaveCustomerInfo(int Login, string Password)
+        {
+            Properties.Settings.Default.Login = Login;
+            Properties.Settings.Default.Password = Password;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ EmexApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EmexApp.EmExDictionaries;
+
+namespace EmexApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpbateClientsList();
+        }
+
+        void UpbateClientsList()
+        {
+            EmExDictionaries.EmExDictionaries emexServices = new EmExDictionaries.EmExDictionaries();
+            EmExDictionaries.ConsumerInfo[] clientsList = emexServices.GetConsumersDict(112, "341292ast", null);
+            foreach  (EmExDictionaries.ConsumerInfo elements in clientsList)
+            {
+                cbClients.Items.Add(elements.UserLogo);
+            }
+        }
+    }
+}
